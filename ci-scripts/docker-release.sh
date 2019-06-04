b742e2b41cea8804fc16793b353ef561039a75c7
Janos Gyerik <info+kronos@janosgyerik.com>
2019-06-04 11:21:13 +0200

    Tag docker images with dev and qa during development

--- ci-scripts/docker-release.sh
+++ ci-scripts/docker-release.sh
@@ -2,16 +2,59 @@
 #
 # Release to dockerhub.
 #
+# On master, we push both a :latest tag, and :<next-version>
+# On release/<foo> branches, we only push :<next-version>
+#
 # Required globals:
 #   DOCKERHUB_USERNAME
 #   DOCKERHUB_PASSWORD
+#   BITBUCKET_BRANCH
+#   BITBUCKET_BUILD_NUMBER
+#
+# Arguments:
+#   REPOSITORY: The dockerhub repository we are pushing to.
+#
 
-set -ex
+set -euo pipefail
 
-IMAGE=$1
-VERSION=$(semversioner current-version)
+REPOSITORY=$1
+VERSION=$(cat next.version)
 
-echo ${DOCKERHUB_PASSWORD} | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
-docker build -t ${IMAGE} .
-docker tag ${IMAGE} ${IMAGE}:${VERSION}
-docker push ${IMAGE}
+# Login to dockerhub.
+docker_login() {
+    docker login --username "$DOCKERHUB_USERNAME" --password-stdin <<< "$DOCKERHUB_PASSWORD"
+}
+
+# Generate all tags.
+generate_tags() {
+    if [[ "${BITBUCKET_BRANCH}" == "master" ]]; then
+        tags=("latest" "${VERSION}")
+    elif [[ "${BITBUCKET_BRANCH}" =~ "release-" ]]; then
+        tags=("${VERSION}")
+    elif [[ "${BITBUCKET_BRANCH}" =~ "qa-" ]]; then
+        tags=("${VERSION}-qa-${BITBUCKET_BUILD_NUMBER}")
+    elif [[ "${BITBUCKET_BRANCH}" =~ "feature/" ]]; then
+        tags=("${VERSION}-dev-${BITBUCKET_BUILD_NUMBER}")
+    else
+        echo "ERROR: Can only push docker images from master, qa-* or release-* branches."
+        exit 1
+    fi
+}
+
+# Build and tag with an intermediate tag.
+docker_build() {
+    docker build -t ${REPOSITORY}:pipelines-${BITBUCKET_BUILD_NUMBER} .
+}
+
+# Tag with final tags and push.
+docker_push() {
+    for tag in "${tags[@]}"; do
+        docker tag "${REPOSITORY}:pipelines-${BITBUCKET_BUILD_NUMBER}" "${REPOSITORY}:${tag}"
+        docker push "${REPOSITORY}:${tag}"
+    done
+}
+
+docker_login
+generate_tags
+docker_build
+docker_push
