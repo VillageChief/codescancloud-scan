41fbb8d4a3f2a28e558561e8c55e2deb32f66183
Janos Gyerik <info+kronos@janosgyerik.com>
2019-06-04 11:33:48 +0200

    Use manual versioning instead of automatic with semversioner

---
+++ ci-scripts/git-tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Tag the current branch with the next
+# version number, push the tag back to the remote.
+
+set -e
+
+# Tag and push
+tag=$(cat next.version)
+if [[ "${BITBUCKET_BRANCH}" =~ "qa-" ]]; then
+    tag="${tag}-qa-${BITBUCKET_BUILD_NUMBER}"
+fi
+
+git tag -a -m "Tagging for release ${tag}" "${tag}"
+git push origin ${tag}
