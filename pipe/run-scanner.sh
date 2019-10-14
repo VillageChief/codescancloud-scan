56fa8205df0aa022f8966ef5491f01a9e3f595df
Ben van Klinken <ben@villagechief.com>
2019-10-14 14:08:00 +1000

    Fixes

--- pipe/run-scanner.sh
+++ pipe/run-scanner.sh
@@ -7,12 +7,22 @@ SCANNER_REPORT="${BITBUCKET_PIPE_STORAGE_DIR}/codescancloud-scan.log"
 parse_environment_variables() {
   EXTRA_ARGS=${EXTRA_ARGS:=""}
   SONAR_TOKEN=${SONAR_TOKEN:?'SONAR_TOKEN variable missing.'}
+  SONAR_ORGANIZATION=${SONAR_ORGANIZATION:?'SONAR_ORGANIZATION variable missing.'}
   BITBUCKET_CLONE_DIR=${BITBUCKET_CLONE_DIR:?'BITBUCKET_CLONE_DIR variable missing.'}
 }
 
 parse_environment_variables
 
 IFS=$'\n' ALL_ARGS=( $(xargs -n1 <<<"${EXTRA_ARGS}") )
+
+ALL_ARGS+=( "-Dsonar.host.url=https://app.codescan.io" )
+ALL_ARGS+=( "-Dsonar.organization=${SONAR_ORGANIZATION}" )
+ALL_ARGS+=( "-Dsonar.login=${SONAR_TOKEN}" )
+ALL_ARGS+=( "-Dsonar.branch.name=${BITBUCKET_BRANCH}" )
+
+if [[ "${BITBUCKET_PR_DESTINATION_BRANCH}" != "" ]]; then
+	ALL_ARGS+=( "-Dsonar.branch.target=${BITBUCKET_PR_DESTINATION_BRANCH}" )
+fi
 
 if [[ "${DEBUG}" == "true" ]]; then
     ALL_ARGS+=( "-Dsonar.verbose=true" )
